bea561fcec7f585f4f81054a221d6fad4e6f79ed
Andres <abedandres@gmail.com>
2019-04-22 20:38:33 -0500

    init

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,45 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require underscore
+//= require gmaps/google
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require jquery3
+//= require popper
+//= require bootstrap
+//= require_tree .
+
+
+$(document).ready( () => {
+/*  let collapseQuestionButtons = Array.prototype.slice.call(document.getElementsByClassName("questions-collapse-button"));
+  collapseQuestionButtons.forEach(button =>{    
+    $(button).on('click', function() {
+      let node = this.childNodes[0]
+      let closed = "ion-ios-plus-empty"
+      let opened = "ion-ios-minus-empty"
+      node.className === closed ? node.className = opened : node.className = closed;   
+    });
+  });
+
+  let collapsePackageButtons = Array.prototype.slice.call(document.getElementsByClassName("collapse-arrow-button"));
+  collapsePackageButtons.forEach(button =>{    
+    $(button).on('click', function() {
+      let node = this
+      let closed = "ion-ios-arrow-down collapse-arrow-button"
+      let opened = "ion-ios-arrow-up collapse-arrow-button"
+      node.className === closed ? node.className = opened : node.className = closed;   
+    });
+  });
+*/
+})
